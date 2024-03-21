bde4b1c0bbd76df12635dc38d9272ae5634ca3f2
Vinnícius kelmy <kelmy.vk@gmail.com>
2024-03-20 21:11:33 -0300

    Done dependency injection with dagger hilt

---
+++ app/src/main/java/com/vk/expensetracker/feature_expensetracker/ExpenseApplication.kt
@@ -0,0 +1,8 @@
+package com.vk.expensetracker.feature_expensetracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExpenseApplication: Application() {
+}
