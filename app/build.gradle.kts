30baaf8fbb3e2a7d2cf89c2c081b8923ed58227f
vk <vk@vk.lan>
2025-09-02 16:53:31 -0300

    Dynamic color bug fix.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-
     // Navigation Compose
     implementation(libs.androidx.navigation.compose)
 
